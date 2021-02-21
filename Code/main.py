87e05eab6018aed4901029f8de6f76ab9b50ee9d
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-02-21 22:51:45 +0100

    Add files via upload

---
+++ Code/main.py
@@ -0,0 +1,23 @@
+import pycom
+import time
+from machine import UART
+
+uart = UART(1)
+uart.init(9600, bits=8, parity=None, stop=1, timeout_chars=100, pins=('P3', 'P4'))
+
+while True:
+    header_bytes = uart.read(1)
+    while(header_bytes != b'\xff'):
+        header_bytes=uart.read(1)
+
+    data_high = int(uart.read(1)[0])
+    data_low = int(uart.read(1)[0])
+    data_sum = int(uart.read(1)[0])
+
+    sum = data_high + data_low
+    if(sum == data_sum - 1):
+        distance = data_high * 256 + data_low
+        distance2 = (distance)/10
+        disTxt = '{:6.2f}'.format(distance)
+        disTxt2 = '{:6.2f}'.format(distance2)
+        print("De afstand is" + disTxt2 + " cm" + "of" + disTxt + " mm")
