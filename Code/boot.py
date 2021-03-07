6d093d88afd61fc2220a76f945e96cd5f7c8a0ba
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-03-07 21:33:10 +0100

    Update boot.py

--- Code/boot.py
+++ Code/boot.py
@@ -1,14 +1,1 @@
-from network import WLAN
-import pycom
-import machine
-wlan = WLAN(mode=WLAN.STA)
-pycom.heartbeat(False)
 
-#wlan.connect(ssid='IoT', auth=(WLAN.WPA2, 'KdGIoT92!'))
-wlan.connect(ssid='Pretty fly for a wifi', auth=(WLAN.WPA2, 'discobar'))
-
-while not wlan.isconnected():
-    pycom.rgbled(0xFF00FF)
-print("WiFi connected succesfully")
-pycom.rgbled(0x3B7A57)
-print(wlan.ifconfig())
