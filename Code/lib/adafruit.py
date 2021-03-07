8cf04ac50c884b8f8f9f614173184c95b5732401
Tibo Smet <74244876+TiboSmetKdG@users.noreply.github.com>
2021-03-07 21:32:00 +0100

    Add files via upload

---
+++ Code/lib/adafruit.py
@@ -0,0 +1,17 @@
+import request
+import time
+
+username = 'TiboKdG'
+feed_name = 'lopy-ultasonic'
+aio_key = 'aio_Tlbl69WF7kO6pICPhn99zzqUuymE'
+url = 'https://io.adafruit.com/api/v2/' + username + '/feeds/' + feed_name + '/data'
+
+def sendDataWifi(data):
+    body = {'value': str(data)}
+    headers = {'X-AIO-Key': aio_key, 'Content-Type': 'application/json'}
+    try:
+        r = request.post(url, json=body, headers=headers)
+        print(r.text)
+        time.sleep(10)
+    except Exception as e:
+        print(e)
---
+++ Code/lib/wifi.py
@@ -0,0 +1,35 @@
+from network import WLAN
+import machine
+import pycom
+import time
+import request
+
+def connectSchool():
+    wlan = WLAN(mode=WLAN.STA)
+    pycom.heartbeat(False)
+
+    wlan.connect(ssid='IoT', auth=(WLAN.WPA2, 'KdGIoT92!'))
+
+    while not wlan.isconnected():
+        time.sleep(2)
+        print("WiFi not connected")
+        pycom.rgbled(0xFF0000)
+    print("WiFi connected succesfully")
+    pycom.rgbled(0x00FF00)
+    time.sleep(2)
+    print(wlan.ifconfig())
+
+def connectHome():
+    wlan = WLAN(mode=WLAN.STA)
+    pycom.heartbeat(False)
+
+    wlan.connect(ssid='Pretty fly for a wifi', auth=(WLAN.WPA2, 'discobar'))
+
+    while not wlan.isconnected():
+        time.sleep(2)
+        print("WiFi not connected")
+        pycom.rgbled(0xFF0000)
+    print("WiFi connected succesfully")
+    pycom.rgbled(0x00FF00)
+    time.sleep(2)
+    print(wlan.ifconfig())
